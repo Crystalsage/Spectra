749a73878116a0a05cdee253f842a154f99490bc
Bourbon <amitkulkarni2028@gmail.com>
2023-07-03 13:55:15 +0530

    Implement multithreading

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ mod utility;
 mod camera;
 mod material;
 
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Instant;
+
 use crate::camera::Camera;
 use crate::hittable::Hittables;
 use crate::material::Material;
@@ -72,9 +76,6 @@ fn main() {
     let max_depth = 50;
 
     
-    // World
-    let mut world: Hittables<Sphere> = random_world();
-
     // Camera
     let lookfrom = Point3::new(13.0, 2.0, 3.0);
     let lookat = Point3::new(0.0, 0.0, 0.0);
@@ -82,26 +83,65 @@ fn main() {
     let dist_to_focus = 10.0;
     let aperture = 0.1;
 
-    let cam = Camera::new(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus);
+    let cam = Arc::new(Camera::new(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus));
 
-    let mut pixels: Pixels = vec![vec![0_i64; width as usize]; height as usize];
+    // World
+    let world: Arc<Hittables<Sphere>> = Arc::new(random_world());
 
-    // And.... render....!
-    for y in 0..height {
-        println!("Scan lines remaining: {}", height-y);
-        for x in 0..width {
-            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
-            for _ in 0..samples_per_pixel {
-                let u: f64 = (x as f64 + random_f64(None, None)) / (width - 1) as f64;
-                let v: f64 = ((height - y) as f64 + random_f64(None, None)) / (height - 1) as f64;
-                let ray: Ray = cam.get_ray(u, v);
-                pixel_color += make_ray_color(ray, &world, max_depth);
+    // Pixel data
+    let pixels: Arc<Mutex<Pixels>> = Arc::new(Mutex::new(vec![vec![0_i64; width as usize]; height as usize]));
 
+    let worker_count = 12;
+    let mut results = vec![];
+
+    let st = Instant::now();
+
+    for i in 0..worker_count {
+        let inner_pixels = pixels.clone();
+        let inner_world = world.clone();
+        let inner_cam = cam.clone();
+
+        // And.... render....!
+        results.push(thread::spawn(move || {
+            for y in 0..height {
+                // println!("Scan lines remaining: {}", height-y);
+                for x in (i * width/worker_count)..((i+1) * width/worker_count) {
+                    let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+                    for _ in 0..samples_per_pixel {
+                        let u: f64 = (x as f64 + random_f64(None, None)) / (width - 1) as f64;
+                        let v: f64 = ((height - y) as f64 + random_f64(None, None)) / (height - 1) as f64;
+                        let ray: Ray = inner_cam.get_ray(u, v);
+
+                        pixel_color += make_ray_color(ray, &*inner_world, max_depth);
+                    }
+
+                    let mut inner_pixels = inner_pixels.lock().unwrap();
+                    inner_pixels[y][x] = make_color(pixel_color, samples_per_pixel);
+                }
             }
-            pixels[y][x] = make_color(pixel_color, samples_per_pixel);
-        }
+        }));
     }
+
+    for handle in results {
+        handle.join().unwrap();
+    }
+
+    let pixels = Arc::try_unwrap(pixels).unwrap().into_inner().unwrap();
+
+    let duration = st.elapsed();
+    let seconds = duration.as_secs() % 60;
+    let minutes = (duration.as_secs() / 60) % 60;
+    let hours = (duration.as_secs() / 60) / 60;
+    println!("Done calculating pixels in {}h:{}m:{}s", hours, minutes, seconds);
+
+    let st = Instant::now();
 
     let image = Image::new(width, height, pixels);
     assert!(matches!(image.write_to_file(file_path), Ok(())));
+
+    let duration = st.elapsed();
+    let seconds = duration.as_secs() % 60;
+    let minutes = (duration.as_secs() / 60) % 60;
+    let hours = (duration.as_secs() / 60) / 60;
+    println!("Wrote to image in {}:{}:{}", hours, minutes, seconds);
 }
